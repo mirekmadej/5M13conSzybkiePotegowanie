dc0a8cbdac5225001a8b0a10e731339a3a7e1752
Mirosław Madej <M.Madej@zsp3zamosc.pl>
2024-11-20 13:53:09 +0100

    Dodaj pliki projektów.

---
+++ 5M13conSzybkiePotegowanie/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _5M13conSzybkiePotegowanie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            Console.WriteLine(szybkaPotega(4,6));
+        }
+        private static int szybkaPotega(int liczba, int wyk)
+        {
+            if (wyk == 1) return liczba;
+            if (wyk % 2 == 0)
+            {
+                wyk = wyk / 2;
+                int w = szybkaPotega(liczba, wyk);
+                return w * w;
+            }
+            else
+                return liczba * szybkaPotega(liczba, wyk - 1);
+        }
+    }
+}
